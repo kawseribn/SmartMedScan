ec63f422884a1a382c77421c26d900c9d1e148e3
Ibna Kowsar <ikowsar@macbookp01zq6l4.lan>
2023-11-29 07:08:15 -0600

    frontend backend updaterd

--- app.py
+++ app.py
@@ -17,13 +17,37 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'  # Folder to store uploaded image
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Max upload size, here it's set to 16MB
 # Configure Flask-Session or other session settings if needed
 app.config['SESSION_TYPE'] = 'filesystem'  # Example for using file-based sessions
+import os
 
 Session(app)
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+# Set Tesseract path (update this to your Tesseract path)
+os.environ['TESSDATA_PREFIX'] = '/Users/ikowsar/anaconda3/envs/flask_env/share/tessdata/'
 
-# Initialize MiniBERT for Question Answering
-# qa_pipeline = pipeline('question-answering', model="prajjwal1/bert-mini", tokenizer="prajjwal1/bert-mini")
-# qa_pipeline = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
-nlp = pipeline('question-answering', model="prajjwal1/bert-mini", tokenizer="prajjwal1/bert-mini")
+# Initialize T5 for Summarization
+summarization_tokenizer = T5Tokenizer.from_pretrained('t5-small')
+summarization_model = T5ForConditionalGeneration.from_pretrained('t5-small')
+
+def is_image_file(filename):
+    return filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
+
+def extract_text(image_path):
+    try:
+        image = Image.open(image_path)
+        return pytesseract.image_to_string(image)
+    except Exception as e:
+        print("Error in OCR extraction:", e)
+        return None
+
+def summarize_text(text):
+    if len(text.split()) < 5:  # Minimum length check
+        return text
+
+    inputs = summarization_tokenizer("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
+    summary_ids = summarization_model.generate(inputs['input_ids'], max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = summarization_tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
+
 
 # Other functions (hash_password, get_db_connection, register_user, check_user)
 # Function to hash passwords
@@ -33,7 +57,7 @@ def hash_password(password):
 
 # Function to get a database connection
 def get_db_connection():
-    conn = sqlite3.connect('medical_app.db', check_same_thread=False)
+    conn = sqlite3.connect('medical_app_new.db', check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -70,9 +94,26 @@ def check_user(username, password):
 
 
 # Function to extract text from an image using OCR
-def extract_text(image):
-    return pytesseract.image_to_string(image)
+# def extract_text(image):
+#     return pytesseract.image_to_string(image)
+def is_image_file(filename):
+    return filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
 
+# def extract_text(image_path):
+#     try:
+#         # Load the image from the file path
+#         image = Image.open(image_path)
+
+#         # Convert the image to the expected format
+#         if image.format == 'WEBP':
+#             image = image.convert('RGB')
+
+#         # Extract text using PyTesseract
+#         extracted_text = pytesseract.image_to_string(image)
+#         return extracted_text
+#     except Exception as e:
+#         print("Error in OCR extraction:", e)
+#         return "OCR extraction error"
 
 def get_current_user_details():
     username = session.get('username')
@@ -116,63 +157,71 @@ def patient_profile():
 @app.route('/patient_home', methods=['GET', 'POST'])
 def patient_home():
     if 'username' not in session or session.get('role') != 'patient':
-        # User not logged in as patient, redirect to login
         return redirect(url_for('login'))
-    
+
+    # Ensure username is correctly fetched from session
     username = session.get('username')
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Retrieve patient's ID based on username
+    # Fetch patient's ID using username
     cursor.execute("SELECT id FROM users WHERE username = ?", (username,))
     patient_id = cursor.fetchone()['id']
-    # Query to retrieve booked appointments for the logged-in patient
+
+    # Fetch appointments for the logged-in patient
     cursor.execute("""
         SELECT a.date, a.time_slot, d.name, d.specialization 
         FROM appointments a
         JOIN doctors d ON a.doctor_id = d.id
         WHERE a.patient_id = ?
     """, (patient_id,))
-    
+
     appointments = cursor.fetchall()
+    print("Patient ID:", patient_id)
+    print("Appointments:", appointments)
+
     conn.close()
 
+
     extracted_text = session.get('extracted_text', '')
+    summary = session.get('summary', '')
     answer = session.get('answer', '')
     image_path = session.get('image_path', '')
 
     if request.method == 'POST':
         if 'image' in request.files:
             image_file = request.files['image']
-            if image_file and image_file.filename != '':
+            if image_file and is_image_file(image_file.filename):
                 filename = secure_filename(image_file.filename)
-                full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                image_file.save(full_path)
-                session['image_path'] = filename  # Store just the filename
-                session.pop('extracted_text', None)  # Clear previous extracted text
-                session.pop('answer', None)  # Clear previous answer
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                image_file.save(image_path)
 
-        if 'extract_text' in request.form and image_path:
-            full_path = os.path.join(app.config['UPLOAD_FOLDER'], image_path)
-            try:
-                image = Image.open(full_path)
-                extracted_text = extract_text(image)
-                print(extracted_text)
-                session['extracted_text'] = extracted_text
-                session.pop('answer', None)  # Clear previous answer
-            except Exception as e:
-                print("Error in extraction:", e)
+                extracted_text = extract_text(image_path)
+                if extracted_text:
+                    summary = summarize_text(extracted_text)
+                    session['extracted_text'] = extracted_text
+                    session['summary'] = summary
+                    print("Extracted Text: ", extracted_text)  # Debug print
+                    print("Summary: ", summary)  # Debug print
+                else:
+                    flash('Failed to extract text from image', 'error')
+            else:
+                flash('Invalid file format', 'error')
+        else:
+            flash('No file selected', 'error')
 
-        elif 'ask_question' in request.form and extracted_text:
-            question = request.form['question']
-            if question:
-                answer = nlp({'question': question, 'context': extracted_text})['answer']
-                session['answer'] = answer
-
+    # Add debug prints to check session variables
+    print("Session Extracted Text: ", session.get('extracted_text', 'No text'))
+    print("Session Summary: ", session.get('summary', 'No summary'))
+    session.pop('extracted_text', None)
+    session.pop('summary', None)
     return render_template('patient_home.html', 
                            image_path=image_path, 
                            extracted_text=extracted_text, 
-                           answer=answer, appointments=appointments)
+                           summary=summary, 
+                           answer=answer, 
+                           appointments=appointments)
+
 
 def get_doctors_list():
     conn = get_db_connection()
@@ -219,34 +268,35 @@ def doctors_list():
 @app.route('/book_appointment', methods=['GET', 'POST'])
 def book_appointment():
     from datetime import datetime
-    # Check if user_id is in session
+
+    # Ensure the user is logged in and has a patient role
     if 'user_id' not in session:
         flash('Please log in to book an appointment', 'error')
         return redirect(url_for('login'))
 
     if request.method == 'POST':
+        # Retrieve patient ID from session and other details from the form
         patient_id = session['user_id']
         chosen_time_slot = request.form['time_slot']
         doctor_id = request.form.get('doctor_id')
-        appointment_date = datetime.now().date()  # Get today's date
+        appointment_date = datetime.now().date()  # Use current date or a selected date
 
-
-        # Save the appointment in the database
+        # Connect to the database and insert the appointment
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO appointments (doctor_id, patient_id, time_slot, date) VALUES (?, ?, ?, ?)",
-                       (doctor_id, patient_id, chosen_time_slot, appointment_date))
-        conn.commit()
-        conn.close()
+        try:
+            cursor.execute("INSERT INTO appointments (patient_id, doctor_id, date, time_slot) VALUES (?, ?, ?, ?)",
+                           (patient_id, doctor_id, appointment_date, chosen_time_slot))
+            conn.commit()
+            flash('Appointment booked successfully!', 'success')
+        except sqlite3.Error as e:
+            return redirect(url_for('doctors_list'))
+        finally:
+            conn.close()
 
-        flash('Appointment booked successfully!', 'success')
         return redirect(url_for('patient_home'))
-
-    # Retrieve doctor details and available time slots
-    doctor = get_doctor_details(doctor_id)
-    time_slots = get_available_time_slots(doctor_id)
-
-    return render_template('book_appointment.html', doctor=doctor, time_slots=time_slots)
+    # If the form is not submitted, redirect to a relevant page or show a message
+    return redirect(url_for('doctors_list'))
 
 def get_doctor_details(doctor_id):
     conn = get_db_connection()
@@ -281,57 +331,212 @@ def get_available_time_slots(doctor_id):
 
 
 
-@app.route('/doctor_home')
+@app.route('/doctor_home', methods=['GET', 'POST'])
 def doctor_home():
-    # Add logic specific to doctor home page
-    return render_template('doctor_home.html')
+    if 'username' not in session or session.get('role') != 'doctor':
+        return redirect(url_for('login'))
+
+    doctor_username = session.get('username')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # Fetch doctor's ID using username
+    cursor.execute("SELECT id FROM users WHERE username = ?", (doctor_username,))
+    doctor_data = cursor.fetchone()
+    # print(doctor_data)
+    # Ensure that the doctor data is available
+    if doctor_data is None:
+        flash("Doctor data not found.", "error")
+        return redirect(url_for('login'))
+
+    doctor_id = doctor_data['id']
+    print(doctor_id)
+    # Fetch booked appointments for the logged-in doctor
+    cursor.execute("""
+        SELECT id, date, time_slot, patient_id, doctor_id
+        FROM appointments
+        WHERE doctor_id = ?
+    """, (doctor_id,))
+
+    appointments = cursor.fetchall()
+    print(appointments)
+    conn.close()
+
+    return render_template('doctor_home.html', appointments=appointments)
+
+
+
+
 
 @app.route('/admin_home')
 def admin_home():
-    # Add logic specific to admin home page
-    return render_template('admin_home.html')
+    # Ensure the user is logged in and has an admin role
+    if 'username' not in session or session.get('role') != 'admin':
+        return redirect(url_for('login'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+    # Fetch data to display on the admin dashboard
+    # Example: count of users, doctors, and appointments
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users")
+    users_count = cursor.fetchone()[0]
+    cursor.execute("SELECT COUNT(*) FROM doctors")
+    doctors_count = cursor.fetchone()[0]
+    cursor.execute("SELECT COUNT(*) FROM appointments")
+    appointments_count = cursor.fetchone()[0]
+    conn.close()
+
+    return render_template('admin_home.html', users_count=users_count, doctors_count=doctors_count, appointments_count=appointments_count)
+
+
+from werkzeug.security import generate_password_hash
+
+def create_password_hash(password):
+    return generate_password_hash(password, method='sha256')
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if not username or not password:
-            flash("Username or password not provided", "error")
-            return redirect(url_for('login'))
+        username = request.form['username']
+        password_hash = generate_password_hash(request.form['password'])
+        role = request.form['role']
+        email = request.form['email']
+        phone = request.form['phone']
+        blood_group = request.form['blood_group']
+        age = request.form['age']
 
-        user = check_user(username, password)
-        if user:
-            session['user_id'] = user['id']
-            session['username'] = user['username']
-            session['role'] = user['role']
-            return redirect(url_for('redirect_to_home'))
-        else:
-            flash("Incorrect username or password.", "error")
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, password_hash, role, email, phone, blood_group, age) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                       (username, password_hash, role, email, phone, blood_group, int(age)))
+        conn.commit()
+        conn.close()
 
-    return render_template('login.html')
+        return redirect(url_for('admin_home'))
+
+    # Display the form for adding a user
+    return render_template('add_user_form.html')
+
+
+@app.route('/delete_user/<int:user_id>')
+def delete_user(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_home'))
+
+
+@app.route('/add_doctor', methods=['GET', 'POST'])
+def add_doctor():
+    if request.method == 'POST':
+        name = request.form['name']
+        specialization = request.form['specialization']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO doctors (name, specialization) VALUES (?, ?)", (name, specialization))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('admin_home'))
+
+    # Display the form for adding a doctor
+    return render_template('add_doctor_form.html')
+
+@app.route('/delete_doctor/<int:doctor_id>')
+def delete_doctor(doctor_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM doctors WHERE id = ?", (doctor_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_home'))
+
+
+@app.route('/add_appointment', methods=['GET', 'POST'])
+def add_appointment():
+    if request.method == 'POST':
+        patient_id = request.form['patient_id']
+        doctor_id = request.form['doctor_id']
+        date = request.form['date']
+        time_slot = request.form['time_slot']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO appointments (patient_id, doctor_id, date, time_slot) VALUES (?, ?, ?, ?)", 
+                       (patient_id, doctor_id, date, time_slot))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('admin_home'))
+
+    # Display the form for adding an appointment
+    return render_template('add_appointment_form.html')
+
+@app.route('/delete_appointment/<int:appointment_id>')
+def delete_appointment(appointment_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM appointments WHERE id = ?", (appointment_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('admin_home'))
+
+# Additional routes for specific admin tasks, like managing users, doctors, etc.
+# These might include routes for adding/editing/deleting users, viewing detailed reports, etc.
 
 
 
 
+def register_doctor(username, password, name, specialization):
+    conn = get_db_connection()
+    c = conn.cursor()
+    try:
+        # First, register the user
+        c.execute("INSERT INTO users (username, password_hash, role) VALUES (?, ?, 'doctor')", (username, hash_password(password)))
+        user_id = c.lastrowid  # Fetch the last inserted id
+
+        # Then, add entry to the doctors table
+        c.execute("INSERT INTO doctors (user_id, name, specialization) VALUES (?, ?, ?)", (user_id, name, specialization))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        conn.close()
+        return False
+    conn.close()
+    return True
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # ... [Existing registration code]
+
     if request.method == 'POST':
         new_username = request.form['new_username']
         new_password = request.form['new_password']
         role = request.form['role']
 
-        # Check if the username already exists
         if username_exists(new_username):
             flash("Username already exists.", "error")
             return redirect(url_for('register'))
 
-        if register_user(new_username, new_password, role):
-            flash("User registered successfully.", "success")
-            return redirect(url_for('login'))
+        if role == 'doctor':
+            # Handle doctor registration
+            doctor_name = request.form.get('doctor_name')
+            specialization = request.form.get('specialization')
+            if register_doctor(new_username, new_password, doctor_name, specialization):
+                flash("Doctor registered successfully.", "success")
+            else:
+                flash("Doctor registration failed.", "error")
         else:
-            flash("Registration failed.", "error")
+            # Handle patient registration
+            if register_user(new_username, new_password, role):
+                flash("User registered successfully.", "success")
+            else:
+                flash("Registration failed.", "error")
+
+        return redirect(url_for('login'))
 
     return render_template('register.html')
 
@@ -370,6 +575,40 @@ def redirect_to_home():
     else:
         # Role is not set, redirect to login
         return redirect(url_for('login'))
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash("Username or password not provided", "error")
+            return render_template('login.html')
+
+        user = check_user(username, password)
+
+        if user:
+            session['user_id'] = user['id']
+            session['username'] = user['username']
+            session['role'] = user['role']
+
+            if user['role'] == 'patient':
+                return redirect(url_for('patient_home'))
+            elif user['role'] == 'doctor':
+                return redirect(url_for('doctor_home'))
+            elif user['role'] == 'admin':
+                return redirect(url_for('admin_home'))
+            else:
+                flash("Invalid role", "error")
+        else:
+            flash("Incorrect username or password.", "error")
+
+    return render_template('login.html')
+
+
+
+
 
 # @app.route('/home', methods=['GET', 'POST'])
 # def home():
